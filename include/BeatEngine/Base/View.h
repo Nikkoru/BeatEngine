c1440658739412df27b41fbc10b7384500010ea3
Nicolas Godoy <nikobenja.gv@gmail.com>
2025-11-09 23:38:31 -0300

    :cc

--- include/BeatEngine/Base/View.h
+++ include/BeatEngine/Base/View.h
@@ -14,7 +14,7 @@ namespace Base {
 		View(std::type_index id) : ID(id) {}
 		virtual ~View() = default;
 	public:
-		virtual void OnDraw(const sf::RenderWindow* window) = 0;
+		virtual void OnDraw(sf::RenderWindow* window) = 0;
 		virtual void OnSFMLEvent(const std::optional<sf::Event> event) = 0;
 		virtual void OnUpdate() = 0;
 		virtual void OnSuspend();
