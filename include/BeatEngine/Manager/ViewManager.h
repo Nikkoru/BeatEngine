bd3e5c78020816c5471b15dacc36629b6cd35561
Nicolas Godoy <nikobenja.gv@gmail.com>
2025-11-10 05:57:18 -0300

    ed

--- include/BeatEngine/Manager/ViewManager.h
+++ include/BeatEngine/Manager/ViewManager.h
@@ -21,12 +21,32 @@ public:
 public:
 	template<typename TView>
 		requires(std::is_base_of_v<Base::View, TView>)
-	void PushView();
+	void PushView() {
+		if (!ViewStack.empty()) {
+			auto ID = std::type_index(typeid(TView));
+
+			if (ViewFabrics.contains(ID)) {
+				ViewStack.push(ViewFabrics[ID]());
+				MainView = ViewStack.top()->ID;
+
+				Logger::GetInstance()->AddInfo(std::format("{} pushed!", typeid(TView).raw_name()), typeid(ViewManager));
+			}
+			else
+				Logger::GetInstance()->AddError("View not registed. You need to register the view with RegisterView<TView>()", typeid(ViewManager));
+		}
+		else
+			Logger::GetInstance()->AddError("No fabrics found", typeid(ViewManager));
+	}
 	void PopView();
 
 	template<typename TView>
 		requires(std::is_base_of_v<Base::View, TView>)
-	void RegisterView();
+	void RegisterView() {
+		auto ID = std::type_index(typeid(TView));
+		auto fabric = ([ID]() -> std::unique_ptr<Base::View> { return std::make_unique<TView>(ID); });
+
+		Logger::GetInstance()->AddInfo(std::format("Registing {}", typeid(TView).raw_name()), typeid(ViewManager));
+	}
 
 	void OnSFMLEvent(std::optional<sf::Event> event);
 	void OnDraw(sf::RenderWindow* window);
--- src/Manager/EventManager.cpp
+++ src/Manager/EventManager.cpp
@@ -5,36 +5,6 @@
 
 std::shared_ptr<EventManager> EventManager::m_Instance = nullptr;
 
-template<typename TEvent>
-	requires(std::is_base_of_v<Base::Event, TEvent>)
-void EventManager::SubscribeView(std::type_index id, Callback callback) {
-	if (id == typeid(nullptr)) {
-		Logger::GetInstance()->AddWarning("Attempted to subscribe null view to event", typeid(EventManager));
-		return;
-	}
-
-	auto eventID = std::type_index(typeid(TEvent));
-
-	if (!m_ViewSubscribers.contains(id))
-		m_ViewSubscribers[id];
-
-	if (m_ViewSubscribers.at(id).find(eventID) == m_ViewSubscribers.at(id).end()) {
-		m_ViewSubscribers.at(id)[eventID] = {};
-	}
-
-	m_ViewSubscribers.at(id).at(eventID).emplace_back(std::move(callback));
-}
-
-template<typename TEvent>
-	requires(std::is_base_of_v<Base::Event, TEvent>)
-void EventManager::Subscribe(Callback callback) {
-	auto eventID = std::type_index(typeid(TEvent));
-
-	if (!m_Subscribers.contains(eventID))
-		m_Subscribers[eventID];
-	m_Subscribers.at(eventID).emplace_back(std::move(callback));
-}
-
 std::shared_ptr<EventManager> EventManager::GetInstance() {
 	if (m_Instance == nullptr)
 		m_Instance = std::make_shared<EventManager>();
