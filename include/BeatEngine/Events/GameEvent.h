fc6f31d9ebfcfbd54a8b972f607f4e85c103cc85
Nikkoru <nikobenja.gv@gmail.com>
2025-12-12 11:56:38 -0300

    added resized event

--- include/BeatEngine/Events/GameEvent.h
+++ include/BeatEngine/Events/GameEvent.h
@@ -2,6 +2,16 @@
 
 #include "BeatEngine/Base/Event.h"
 
+#include <SFML/System/Vector2.hpp>
+
 class GameSettingsChanged : public Base::Event {
-	GameSettingsChanged() : Base::Event(typeid(GameSettingsChanged)) {}
+public:
+    GameSettingsChanged() : Base::Event(typeid(GameSettingsChanged)) {}
+};
+
+class GameResized : public Base::Event {
+public:
+    sf::Vector2u Size; 
+public:
+    GameResized(sf::Vector2u newSize) : Base::Event(typeid(GameResized)), Size(newSize) {} 
 };
