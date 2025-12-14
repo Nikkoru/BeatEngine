df2dae192922e95dc814dd02ae38ee820929f6e5
Nikkoru <nikobenja.gv@gmail.com>
2025-12-14 19:58:52 -0300

    implemented UIPanel

---
+++ include/BeatEngine/UI/Elements/UIPanel.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "BeatEngine/UI/UIElement.h"
+#include <SFML/Graphics/RenderStates.hpp>
+#include <SFML/Graphics/RenderTarget.hpp>
+
+class UIPanel : public UIElement {
+private:
+    sf::Color m_ColorRect = sf::Color::White;
+    bool m_ShowRect = true;
+public:
+    UIPanel() = default;
+    void OnDraw(sf::RenderTarget& target, sf::RenderStates states) const override;
+    void Update(float dt) override {}
+
+    void ShowRect(bool show);
+};
---
+++ src/UI/Elements/UIPanel.cpp
@@ -0,0 +1,10 @@
+#include "BeatEngine/UI/Elements/UIPanel.h"
+
+void UIPanel::OnDraw(sf::RenderTarget& target, sf::RenderStates states) const {
+   if (m_ShowRect)
+       target.draw(m_LayoutRect);
+}
+
+void UIPanel::ShowRect(bool show) {
+    this->m_ShowRect = show;
+}
